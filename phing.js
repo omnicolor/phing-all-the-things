4e1ce7a7a1a7ce74dd42b95fac274c650cf4a6e9
Omni Adams <oadams@isightpartners.com>
2012-12-10 07:06:30 -0600

    Added basic HTML and JS

---
+++ phing.js
@@ -0,0 +1,11 @@
+$(function() {
+    $.deck('.slide');
+
+    $('#style-themes').change(function() {
+        $('#style-theme-link').attr('href', $(this).val());
+    });
+
+    $('#transition-themes').change(function() {
+        $('#transition-theme-link').attr('href', $(this).val());
+    });
+});
