8cb3706796ea2385f050a325e6987b5c7accce43
Omni Adams <omni@digitaldarkness.com>
2014-01-14 10:48:56 -0600

    Fleshed out New Relic update examples

--- UpdateNewRelicTask.php
+++ UpdateNewRelicTask.php
@@ -3,11 +3,42 @@
 require_once 'phing/Task.php';
 
 class UpdateNewRelicTask extends Task {
-    public function init() {
-        // Runs when task is imported
+    protected $apiKey;
+    protected $appId;
+    protected $url;
+    protected $version;
+
+    public function setApiKey($apiKey) {
+        $this->apiKey = $apiKey;
+    }
+
+    public function setAppId($appId) {
+        $this->appId = $appId;
+    }
+
+    public function setUrl($url) {
+        $this->url = $url;
+    }
+
+    public function setVersion($version) {
+        $this->version = $version;
     }
 
     public function main() {
-        // Runs when task is executed
+        $options = array(
+            CURLOPT_POSTFIELDS => array(
+                'deployment[application_id]' => $this->appId,
+                'deployment[description]' => $this->version,
+                'deployment[user]' => 'Phing task',
+            ),
+            CURLOPT_HTTPHEADER => array(
+                'x-api-key: ' . $this->apiKey,
+            ),
+            CURLOPT_URL => $this->url,
+        );
+        $ch = curl_init();
+        curl_setopt_array($ch, $options);
+        curl_exec($ch);
+        curl_close($ch);
     }
 }
