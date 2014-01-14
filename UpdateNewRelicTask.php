92bb40e74daefc3d1727bb004db3cc161339a913
Omni Adams <omni@digitaldarkness.com>
2014-01-14 09:06:05 -0600

    Added bare example custom Phing task

---
+++ UpdateNewRelicTask.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'phing/Task.php';
+
+class UpdateNewRelicTask extends Task {
+    public function init() {
+        // Runs when task is imported
+    }
+
+    public function main() {
+        // Runs when task is executed
+    }
+}
