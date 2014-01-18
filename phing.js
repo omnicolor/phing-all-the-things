4571b896d8d0f417616bbcaf47cb832350b4da8e
Omni Adams <omni@digitaldarkness.com>
2014-01-18 09:42:17 -0700

    Made slides ready for publishing on mordor

--- phing.js
+++ phing.js
@@ -1,13 +1,6 @@
 $(function() {
     $.deck('.slide');
 
-    var notes = window.open('notes.html', 'notes',
-        'menubar=no,toolbar=no,location=no,dependent=yes,height=800,width=1200');
-
-    $(document).bind('deck.change', function(event, from, to) {
-        displayNotes(notes);
-    });
-
     $('#style-themes').change(function() {
         $('#style-theme-link').attr('href', $(this).val());
     });
@@ -23,16 +16,3 @@ $(function() {
 
     prettyPrint();
 });
-
-
-/**
- * Send the speaker notes to the speaker notes window.
- * @param {string} notes HTML of the notes to send.
- */
-var displayNotes = function(notes) {
-    // Set timeout to give the deck classes time to settle down. Otherwise
-    // .deck-current still points to the slide we're leaving.
-    window.setTimeout(function() {
-        notes.postMessage($('.deck-current .note').html(), '*');
-    }, 0);
-};
